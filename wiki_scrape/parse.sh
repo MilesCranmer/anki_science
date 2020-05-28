2bff4ce64670afbe34c38ac97b155fcc540d9cf1
MilesCranmer <miles.cranmer@gmail.com>
2020-05-27 23:33:33 -0400

    Add ML

--- wiki_scrape/parse.sh
+++ wiki_scrape/parse.sh
@@ -1,2 +1,3 @@
 cat computer_science.txt | vims -r '\[\[' 'd' -t '%s/\]\].*//g' '%s/|.*//g' "%s/\*'''\[\[//g" | sort | uniq > computer_science_terms.txt
 cat astronomy.txt | vims -r '{{term|' 'dd' -t '%s/\[\|\]//g' '%s/.*term|//g' '%s/}}//g' '%s/|.*//g' '%s/wikt://g' '%s/term=//g' | sort | uniq > astronomy_terms.txt
+cat ml.txt | vims -r '{{term|' 'dd' -t '%s/\[\|\]//g' '%s/.*term|//g' '%s/}}//g' '%s/|.*//g' '%s/wikt://g' '%s/term=//g' | sort | uniq > ml_terms.txt
